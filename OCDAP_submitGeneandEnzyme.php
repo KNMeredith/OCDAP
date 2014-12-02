e096952e38e62b2a9c9aca033b06f7e888e9102d
KNMeredith <nelsonka@email.meredith.edu>
2014-12-02 12:31:30 -0500

    Has examples of what you can input

--- OCDAP_submitGeneandEnzyme.php
+++ OCDAP_submitGeneandEnzyme.php
@@ -7,8 +7,8 @@
 	<body>
 		<h2> OCDAP: Gene and Enzyme Form </h2>
 		<form action="substringsearch_grab.php" method="POST">
-			Gene ID: <input name='geneID'  /> <br />
-			Restriction Enzyme: <input name='resEnzyme'  /> <br />
+			Gene ID: <input name='geneID'  /> e.g. 24182471 <br /> 
+			Restriction Enzyme: <input name='resEnzyme'  /> e.g. AluI [OR] TaqI <br />
 			<input type="submit" value="Submit"> <br> 
 		</form>
 	</body>
