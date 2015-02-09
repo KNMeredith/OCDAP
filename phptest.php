e8d08f447ecfaa725c12817074e28ba3a8d1c837
KNMeredith <nelsonka@email.meredith.edu>
2015-02-09 11:21:35 -0500

    Testing

---
+++ phptest.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+php test page <br />
+
+<?php
+
+echo "If you see this, PHP is running! ";
+
+?>
+<br />
+If you do not see a message above that PHP is running,
+then it isn't.
+
+</body>
+</html>
+
+
